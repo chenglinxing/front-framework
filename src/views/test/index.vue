3c52cc79ef07fe57e8a23e20f0bcb586896c8586
chenglx <1428054498@qq.com>
2023-12-26 18:27:17 +0800

    test

---
+++ src/views/test/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home-index">
+    <div class="header-info">
+      <div class="header-title"><span>海南自贸港物流监管服务系统</span></div>
+
+      <div class="header-menu-user"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import {} from "vue";
+</script>
+
+<style lang="scss" scoped>
+.home-index{
+    height: 100%;
+}
+</style>
